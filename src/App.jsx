ae1b774a72b427497ae4e748ced1df2ca9a7b553
Dileep <115636822+ddileep01@users.noreply.github.com>
2024-05-06 16:53:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-500 text-2xl font-semibold">Quick Job Finder</h1>
+        <p>Empowering your career journey.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
